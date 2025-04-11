d7de1e2af41935d0f035fd99cbd007586f836eab
João Garcia <jaofrangarcia@gmail.com>
2025-04-11 19:52:16 -0300

    commit

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -19,9 +19,15 @@
         <div class="signin-signup">
           <form action="#" class="sign-in-form">
             <h2 class="title">Entrar</h2>
+                        
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Empresa" />
+            </div>
+
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="text" placeholder="Usuário" />
+              <input type="Email" placeholder="Email" />
             </div>
 
             <div class="input-field">
@@ -33,27 +39,13 @@
           </form>
 
           <form action="#" class="sign-up-form">
-            <h2 class="title">Registre-se</h2>
-
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="E-mail" />
-            </div>
-
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Usuário" />
-            </div>
-
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Senha" />
-            </div>
-
-            <input type="submit" class="btn" value="Avançar" />
+            <h1 style="text-align: center;">Desenvolvido por: <br> Kaléu K. de Paula e João F. Garcia</h1>
+            <h3>teste</h3>
           </form>
         </div>
       </div>
+
+      <!-- Sobre nós -->
 
       <div class="panels-container">
         <div class="panel left-panel">
@@ -63,9 +55,9 @@
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
               ex ratione. Aliquid!
             </p>
-            <button class="btn transparent" id="sign-up-btn">Registrar-se</button>
+            <button class="btn transparent" id="sign-up-btn">Sobre nós</button>
           </div>
-          <img src="/frontend/assets/images/log.svg" class="image" alt="" />
+          <img src="../../assets/images/log.svg" class="image" alt="" />
         </div>
 
         <div class="panel right-panel">
@@ -77,7 +69,7 @@
             </p>
             <button class="btn transparent" id="sign-in-btn">Entrar</button>
           </div>
-          <img src="/frontend/assets/images/register.svg" class="image" alt="" />
+          <img src="../../assets/images/register.svg" class="image" alt="" />
         </div>
       </div>
     </div>
